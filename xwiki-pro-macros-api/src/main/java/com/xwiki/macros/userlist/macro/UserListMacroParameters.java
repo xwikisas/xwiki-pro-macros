07cb0486110a68cad501ddba86ce93d5ce83406a
slauriere <slauriere@xwiki.com>
2022-08-18 10:40:45 +0200

    #74: Create a user list macro - Add "properties" parameter for controlling the user properties to be   displayed - Display users in a table rather than in a list - Use Object display API

--- xwiki-pro-macros-api/src/main/java/com/xwiki/macros/userlist/macro/UserListMacroParameters.java
+++ xwiki-pro-macros-api/src/main/java/com/xwiki/macros/userlist/macro/UserListMacroParameters.java
@@ -19,12 +19,18 @@
  */
 package com.xwiki.macros.userlist.macro;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.xwiki.properties.annotation.PropertyDescription;
 import org.xwiki.properties.annotation.PropertyDisplayType;
+import org.xwiki.properties.annotation.PropertyName;
 
 public class UserListMacroParameters
 {
     private UserReferenceList users;
+
+    private List<String> properties = Arrays.asList("avatar", "username");
 
     /**
      * @return a list of users
@@ -44,5 +50,25 @@ public class UserListMacroParameters
     public void setUsers(UserReferenceList users)
     {
         this.users = users;
+    }
+
+    /**
+     * @return list of properties to be displayed
+     */
+    public List<String> getProperties()
+    {
+        return this.properties;
+    }
+
+    /**
+     * Sets a list of property names.
+     *
+     * @param properties list of user properties
+     */
+    @PropertyName("Properties")
+    @PropertyDescription("List of user properties to be displayed")
+    public void setProperties(List<String> properties)
+    {
+        this.properties = properties;
     }
 }
