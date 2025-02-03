89dd0faa9b0e32beee8e216cdacccd5d74b74993
ChiuchiuSorin <sorin.chiuchiu@gmail.com>
2025-02-03 14:07:28 +0200

    Display mode of the view file macro changes in edit mode in the wysiwyg editor when adding a new macro #311
    * added a check for target syntax

--- xwiki-pro-macros-api/src/main/java/com/xwiki/macros/viewfile/internal/macro/ViewFileMacro.java
+++ xwiki-pro-macros-api/src/main/java/com/xwiki/macros/viewfile/internal/macro/ViewFileMacro.java
@@ -76,6 +76,10 @@ public class ViewFileMacro extends AbstractProMacro<ViewFileMacroParameters>
 
         scriptContext.setAttribute("params", parameters, ScriptContext.ENGINE_SCOPE);
         scriptContext.setAttribute("isInline", context.isInline(), ScriptContext.ENGINE_SCOPE);
+        if (context.getTransformationContext().getTargetSyntax() != null) {
+            String targetSyntaxId = context.getTransformationContext().getTargetSyntax().getType().getId();
+            scriptContext.setAttribute("targetSyntaxId", targetSyntaxId, ScriptContext.ENGINE_SCOPE);
+        }
         try {
             return this.templateManager.execute(customTemplate).getChildren();
         } catch (Exception e) {
