0e241659cc68053bd4f64f586031e66b6833f97d
Farcasut <92780090+Farcasut@users.noreply.github.com>
2025-03-31 11:24:51 +0300

    Improve the tagList macro to take into accounts users that come from confluence #506  (#507)

--- xwiki-pro-macros-confluence-bridges/xwiki-pro-macros-confluence-bridges-api/src/main/java/com/xwiki/macros/confluence/internal/ConfluenceSpaceUtils.java
+++ xwiki-pro-macros-confluence-bridges/xwiki-pro-macros-confluence-bridges-api/src/main/java/com/xwiki/macros/confluence/internal/ConfluenceSpaceUtils.java
@@ -20,6 +20,7 @@
 package com.xwiki.macros.confluence.internal;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
@@ -29,13 +30,15 @@ import org.xwiki.contrib.confluence.resolvers.ConfluenceResolverException;
 import org.xwiki.contrib.confluence.resolvers.ConfluenceSpaceKeyResolver;
 import org.xwiki.contrib.confluence.resolvers.ConfluenceSpaceResolver;
 import org.xwiki.model.EntityType;
-import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.EntityReference;
 import org.xwiki.model.reference.EntityReferenceResolver;
+import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryException;
+import org.xwiki.query.QueryManager;
 import org.xwiki.stability.Unstable;
 
 import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
 
 /**
  * Tools to manipulate migrated Confluence spaces.
@@ -63,9 +66,16 @@ public class ConfluenceSpaceUtils
     @Inject
     private EntityReferenceResolver<String> resolver;
 
+    @Inject
+    private QueryManager queryManager;
+
+    @Inject
+    @Named("compact")
+    private EntityReferenceSerializer<String> serializer;
+
     /**
-     * This method is meant to handle macro parameters coming from Confluence that could contain both XWiki spaces
-     * or Confluence (pseudo) space keys.
+     * This method is meant to handle macro parameters coming from Confluence that could contain both XWiki spaces or
+     * Confluence (pseudo) space keys.
      *
      * @param spaceKeyOrRef the space key, or "@self", or a XWiki reference to the space.
      * @return the root of the Confluence space described by the parameter, or null if not found.
@@ -77,23 +87,29 @@ public class ConfluenceSpaceUtils
                 return confluenceSpaceResolver.getSpace(contextProvider.get().getDoc().getDocumentReference());
             }
 
-            if (spaceKeyOrRef.indexOf(':') != -1 || spaceKeyOrRef.indexOf('.') != -1) {
-                // This is a XWiki reference
-                EntityReference spaceRef = resolver.resolve(spaceKeyOrRef, EntityType.SPACE);
-                EntityReference webHome = new EntityReference("WebHome", EntityType.DOCUMENT, spaceRef);
-                if (contextProvider.get().getWiki().exists(new DocumentReference(webHome), contextProvider.get())) {
-                    // the home page of this space exists
-                    return spaceRef;
-                }
+            if (spaceKeyOrRef.startsWith("confluenceSpace:")) {
+                // The spaceKeyOrRef might be prefixed with confluenceSpace: when at migration time we didn't know
+                // where the space is. In the meantime it might have been migrated, and we should check if the space
+                // is available in XWiki
+                String unprefixedSpaceKey = spaceKeyOrRef.substring(spaceKeyOrRef.indexOf(':') + 1);
+                return confluenceSpaceKeyResolver.getSpaceByKey(unprefixedSpaceKey);
+            }
+            EntityReference spaceRef = resolver.resolve(spaceKeyOrRef, EntityType.SPACE);
+            Query checkSpaceExistence =
+                queryManager.createQuery("SELECT count(reference) FROM XWikiSpace s where s.reference=:reference",
+                    Query.HQL);
+            checkSpaceExistence.bindValue("reference", serializer.serialize(spaceRef));
+            long count = (long) checkSpaceExistence.execute().get(0);
+            if (count > 0) {
+                return spaceRef;
             }
 
             return confluenceSpaceKeyResolver.getSpaceByKey(spaceKeyOrRef);
         } catch (ConfluenceResolverException e) {
             logger.warn("Could not convert space [{}] to an entity reference", spaceKeyOrRef, e);
-        } catch (XWikiException e) {
-            logger.warn("Could not check document [{}] existence", spaceKeyOrRef, e);
+        } catch (QueryException e) {
+            logger.error("Failed to execute query to check if the [{}] space exists.", spaceKeyOrRef, e);
         }
-
         return null;
     }
 }
