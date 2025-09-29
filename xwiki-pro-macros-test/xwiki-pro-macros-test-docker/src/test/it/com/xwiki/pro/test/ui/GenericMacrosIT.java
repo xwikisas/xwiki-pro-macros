5a874b3e8954ecc2a1166ddd2fc196e223b4fef4
Sarah <sarah.buliga@xwiki.com>
2025-09-29 12:08:09 +0300

    Add functional test for remaining generic pro macros #597 *restored tomcat comfig

--- xwiki-pro-macros-test/xwiki-pro-macros-test-docker/src/test/it/com/xwiki/pro/test/ui/GenericMacrosIT.java
+++ xwiki-pro-macros-test/xwiki-pro-macros-test-docker/src/test/it/com/xwiki/pro/test/ui/GenericMacrosIT.java
@@ -41,6 +41,7 @@ import org.xwiki.test.docker.junit5.ExtensionOverride;
 import org.xwiki.test.docker.junit5.TestConfiguration;
 import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.docker.junit5.servletengine.ServletEngine;
 import org.xwiki.test.ui.TestUtils;
 import org.xwiki.test.ui.po.CommentsTab;
 
@@ -79,11 +80,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @since 1.25.2
  */
 @UITest(
-    /*servletEngine = ServletEngine.TOMCAT, forbiddenEngines = {
+    servletEngine = ServletEngine.TOMCAT, forbiddenEngines = {
     // These tests need to have XWiki running inside a Docker container (we chose Tomcat since it's the most
     // used one), because they need LibreOffice to be installed, and we cannot guarantee that it is installed on the
     // host machine.
-    ServletEngine.JETTY_STANDALONE },*/
+    ServletEngine.JETTY_STANDALONE },
     properties = {
         "xwikiCfgPlugins=com.xpn.xwiki.plugin.tag.TagPlugin, "
             + "com.xpn.xwiki.plugin.skinx.JsResourceSkinExtensionPlugin, "
