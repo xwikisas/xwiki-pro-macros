d5f61f10589a36d9587d56f31722c0a3a65cd9e0
farcasut <farcasialexandrububu@gmail.com>
2025-10-29 23:49:09 +0200

    Details summary has columns values in the wrong place #645

--- xwiki-pro-macros-confluence-bridges/xwiki-pro-macros-confluence-bridges-api/src/main/java/com/xwiki/macros/confluence/detailssummary/internal/macro/DetailsSummaryMacro.java
+++ xwiki-pro-macros-confluence-bridges/xwiki-pro-macros-confluence-bridges-api/src/main/java/com/xwiki/macros/confluence/detailssummary/internal/macro/DetailsSummaryMacro.java
@@ -27,7 +27,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Named;
