e6788a0f42fd69f47e60ccb3a6096b6b03ec0117
Farcasut <92780090+Farcasut@users.noreply.github.com>
2026-07-24 12:45:40 +0300

    UI/UX - User List Macro - Unclear what properties are available #740 (#802)

---
+++ xwiki-pro-macros-api/src/main/java/com/xwiki/macros/script/UserListScriptService.java
@@ -0,0 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.macros.script;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.script.service.ScriptService;
+import org.xwiki.stability.Unstable;
+
+import com.xwiki.macros.userlist.internal.macro.UserListConfiguration;
+
+/**
+ * Expose the configuration to the velocity scripts.
+ *
+ * @version $Id$
+ * @since 1.32.0
+ */
+@Component
+@Named("userlist")
+@Singleton
+@Unstable
+public class UserListScriptService implements ScriptService
+{
+    @Inject
+    private UserListConfiguration userListConfiguration;
+
+    /**
+     * @return a set with the banned fields for the current user.
+     */
+    public Set<String> getBannedFields()
+    {
+        return userListConfiguration.getBannedFields();
+    }
+}
